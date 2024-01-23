535fbbfa6f5ebed643544c90cbabe5428ced4554
Kalyan Raghava Kotala <84460796+Kalyanraghavakotala@users.noreply.github.com>
2024-01-23 23:31:35 +0530

    Time: 2 ms (13.78%), Space: 42 MB (39.56%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -8,6 +8,38 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// class Solution {
+//     public ListNode partition(ListNode head, int x) {
+
+
+//         ListNode list1 = new ListNode(0);
+//         ListNode tail1 = list1;
+//         ListNode list2 = new ListNode(0);
+//         ListNode tail2 = list2;
+//         while(head != null ){
+//             if(head.val < x){
+//                 tail1.next = head;
+//                 tail1 = tail1.next;
+//             }
+//             else{
+//                 tail2.next = head;
+//                 tail2 = tail2.next;
+
+//             }
+//             head = head.next;
+//         }
+//         tail1.next = list2.next;
+//         tail2.next = null;
+
+//         return list1.next;
+        
+//     }
+// }
+
+
+
+
 class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode res=new ListNode(0);
@@ -27,7 +59,7 @@ class Solution {
             temp1=temp1.next;
         }
         temp.next=res1.next; 
-        
+        System.gc();
         return res.next;
     }
 }
