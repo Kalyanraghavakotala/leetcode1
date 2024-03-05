ef4a20f443d4443a8e8ee3b741e4a020a13c6f1d
Kalyan Raghava Kotala <84460796+Kalyanraghavakotala@users.noreply.github.com>
2024-03-05 17:59:29 +0530

    Time: 6 ms (24.76%), Space: 44.8 MB (96.19%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -3,8 +3,8 @@ class Solution {
         int n=s.length();
         int i=0,j=n-1;
         while(i<j && s.charAt(i) == s.charAt(j)){
-            while(i<j && s.charAt(i) == s.charAt(i+1)) i++;
-            while(j>i && s.charAt(j) == s.charAt(j-1)) j--;
+            while(i<n-1 && s.charAt(i) == s.charAt(i+1)) i++;
+            while(j>0 && s.charAt(j) == s.charAt(j-1)) j--;
             i++;
             j--;
             if(i>j) return 0;
